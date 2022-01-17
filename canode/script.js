c4e9f5cd805b420da17954b1da3437a26500928f
unknown <toyinadesina60@gmail.com>
2022-01-17 13:53:38 +0100

    Responsiveness

--- canode/script.js
+++ canode/script.js
@@ -4,13 +4,37 @@ document.addEventListener("DOMContentLoaded", function(){
 // alert("kfkfkkf")
 
 $(document).ready(function(){
+  $("#navBar").mouseenter(function(){
+    $(this).addClass("navbar-light")
+    $(this).removeClass("navbar-dark")
+  });
+
+  $("#navBar").mouseleave(function(){
+    $(this).removeClass("navbar-light")
+    $(this).addClass("navbar-dark")
+  });
+  
+  if($(window).scrollTop()){
+    $("#navBar").addClass("lightBg")
+    $("#navBar").addClass("navbar-light")
+    $("#navBar").removeClass("navbar-dark")
+  }
+  else{
+    $("#navBar").removeClass("lightBg")
+    $("#navBar").removeClass("navbar-light")
+    $("#navBar").addClass("navbar-dark")
+  }
   $(window).scroll(function() {
     // alert("jjjj");
     if($(window).scrollTop()){
       $("#navBar").addClass("lightBg")
+      $("#navBar").addClass("navbar-light")
+      $("#navBar").removeClass("navbar-dark")
     }
     else{
       $("#navBar").removeClass("lightBg")
+      $("#navBar").removeClass("navbar-light")
+      $("#navBar").addClass("navbar-dark")
     }
   });
   $(".flexible").find(".p1").each(function(){
