77ca29d91376d1b3c8e2befac35c6945a9426f76
unknown <toyinadesina60@gmail.com>
2022-07-07 21:47:31 +0100

    contact formll

--- form-to-email.php
+++ form-to-email.php
@@ -1,48 +1,55 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+
+// if(!isset($_POST['submit']))
+// {
+// 	//This page should not be accessed directly. Need to submit the form.
+// 	echo "error; you need to submit the form!";
+// }
+if(!empty($_POST)){
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  try{
+    if(empty($name)||empty($visitor_email)||empty($subject)){
+        $output->status = false;
+        $output->message = "Name, email and subject are mandatory!";
+    }
+    else{
+      $email_from = 'toyinadesina60@gmail.com';//<== update the email address
+      $email_subject = "(New Form submission from Your Web Page)";
+      $email_body = "You have received a new message from the user $name.\n".
+          "Here is the message:\n $message".
+
+      $to = "toyinadesina60@gmail.com";//<== update the email address
+      $headers = "From: $email_from \r\n";
+      $headers .= "Reply-To: $visitor_email \r\n";
+      $headers .= "MIME-Version: 1.0\r\n";
+      $headers .= "Content-type: text/html\r\n";
+      //Send the email!
+      $ref = mail($to, $email_subject, $email_body, $headers);
+
+      if($ref){
+        $output->success = true;
+        $output->message = "succesful";
+      }else{
+        $output->success = false;
+        $output->message = "not succesful";
+      }
+    }
+    print_r(json_encode($output));
+
+  }catch(error $e){
+    print_r($e);
+  }
 }
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-// $subject = $_POST['subject'];
-$message = $_POST['message'];
 
-//Validate first
-if(empty($name)||empty($visitor_email)||empty($subject))
-{
-    echo "Name, email and subject are mandatory!";
-    exit;
-}
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
 
-$email_from = 'toyinadesina60@gmail.com';//<== update the email address
-$email_subject = "(New Form submission from Your Web Page)";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
-
-$to = "toyinadesina60@gmail.com";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html\r\n";
-//Send the email!
-$ref = mail($to, $email_subject, $email_body, $headers);
 //done. redirect to thank-you page.
 // header('Location: thank-you.php');
 
-if($ref){
-  $message->success = true;
-  
-}else{
-  $message->success = false;
-}
+
 
 echo json_encode($message);
 
